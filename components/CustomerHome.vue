8eae21b9a388c298764f589153dbcad76a5b71dc
erdogansonmez <erdogansonmez@outlook.com>
2024-11-20 13:59:37 +0300

    Fix

--- components/CustomerHome.vue
+++ components/CustomerHome.vue
@@ -6,13 +6,13 @@
         <div class="flex justify-start items-center gap-x-2">
           <UAvatar :src="user.profilePicture" size="xl"/>
           <div class="flex flex-col justify-start items-start">
-            <p class="text-xl">{{user.name}} {{user.surname}}</p>
-            <p class="text-sm">{{user.username}}</p>
+            <p class="text-xl">{{ user.name }} {{ user.surname }}</p>
+            <p class="text-sm">{{ user.username }}</p>
           </div>
           <div class="flex justify-end items-center flex-grow flex-1 gap-x-2">
             <NuxtLink to="/notifications">
               <UChip :text="unreadCount" size="3xl">
-            <Icon name="material-symbols:notifications-sharp" size="30" />
+                <Icon name="material-symbols:notifications-sharp" size="30"/>
               </UChip>
             </NuxtLink>
           </div>
@@ -35,13 +35,13 @@
       },
     ]"
         />
-        <img :src="test" alt="">
 
       </div>
       <div class="flex flex-col p-4">
         <p>Favori Rotalarınız</p>
-        <div class="px-4 py-2 bg-gray-800 text-md rounded-md flex justify-between items-center" v-for="favorite in favoriteTrips">
-          <p>{{favorite.name}}</p>
+        <div class="px-4 py-2 bg-gray-800 text-md rounded-md flex justify-between items-center"
+             v-for="favorite in favoriteTrips">
+          <p>{{ favorite.name }}</p>
           <UButton color="neutral" variant="outline">Seç</UButton>
         </div>
       </div>
@@ -54,14 +54,18 @@
 
         <div class="flex flex-col p-4">
           <p class="text-black text-3xl">Aracın Yola Çıktı!</p>
-          <p class="text-black text-md">Hemen aracını canlı olarak takip edebilir, sana varış süresini öğrenebilirsin.</p>
+          <p class="text-black text-md">Hemen aracını canlı olarak takip edebilir, sana varış süresini
+            öğrenebilirsin.</p>
           <div class="flex flex-col gap-y-2 mt-2">
-            <div class="text-black flex gap-x-4 justify-between items-center p-2 rounded-md" v-for="trip in liveTrips.slice(0,1)">
+            <div class="text-black flex gap-x-4 justify-between items-center p-2 rounded-md"
+                 v-for="trip in liveTrips.slice(0,1)">
               <div class="w-1/2 flex flex-col">
-                <p>{{trip.name}}</p>
+                <p>{{ trip.name }}</p>
               </div>
               <div class="w-1/2">
-                <div class="bg-red-800 py-2 text-white text-center rounded-md" @click="goTripDetails(trip.id)">Canlı Takip</div>
+                <div class="bg-red-800 py-2 text-white text-center rounded-md" @click="goTripDetails(trip.id)">Canlı
+                  Takip
+                </div>
               </div>
 
             </div>
@@ -77,7 +81,7 @@
       <p class="text-sm opacity-50" v-if="incomingTrips.length > 0">Yaklaşan Transferleriniz</p>
       <div class="bg-gray-700 flex gap-x-4 justify-between items-center p-2 rounded-md" v-for="trip in incomingTrips">
         <div class="flex flex-col">
-          <p>{{trip.name}}</p>
+          <p>{{ trip.name }}</p>
 
         </div>
 
@@ -93,17 +97,14 @@
 <script setup lang="ts">
 import {TripStatus} from "~/core/api/modules/trip/models/ITripHeaderDto";
 
-const {user} =  storeToRefs(useAuthStore())
-const {unreadCount} = storeToRefs(useNotificationStore())
 let interval: any;
-const test = ref("Gelmedi")
 
-const{
-  tripHeaders
-} = storeToRefs(useCustomerTripStore());
-const {
-  favoriteTrips
-} = storeToRefs(useCreateTransferStore())
+
+const {tripHeaders} = storeToRefs(useCustomerTripStore());
+const {accountVehicleId} = storeToRefs(useVehicleModeStore())
+const {unreadCount} = storeToRefs(useNotificationStore())
+const {user} = storeToRefs(useAuthStore())
+const {favoriteTrips} = storeToRefs(useCreateTransferStore())
 
 const liveTrips = computed(() => tripHeaders.value.filter(x => x.status == TripStatus.Live));
 const incomingTrips = computed(() => tripHeaders.value.filter(x => x.status == TripStatus.Approved))
@@ -121,10 +122,14 @@ onUnmounted(() => {
   clearInterval(interval)
 })
 
-function onVehicleModeClick(){
-  useCamera('qr').then(res => {
-    useVehicleModeStore().setAccountVehicleId(res)
-  })
+function onVehicleModeClick() {
+  if (accountVehicleId.value != ''){
+    useRouter().push('/vehicle-mode')
+  }else{
+    useCamera('qr').then(res => {
+      useVehicleModeStore().setAccountVehicleId(res)
+    })
+  }
 }
 
 
--- stores/useVehicleModeStore.store.ts
+++ stores/useVehicleModeStore.store.ts
@@ -63,6 +63,7 @@ export const useVehicleModeStore = defineStore('useVehicleModeStore', () => {
         accountVehicle,
         trips,
         selectedTrip,
+        accountVehicleId,
 
         setAccountVehicleId,
         setVehicleOnline,
