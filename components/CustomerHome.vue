e2e413c755c6b83ab0b6409b12b201598ba266ad
erdogansonmez <erdogansonmez@outlook.com>
2024-11-23 17:43:37 +0300

    Vehicle Card Mode

--- components/CustomerHome.vue
+++ components/CustomerHome.vue
@@ -4,7 +4,7 @@
 
       <div class="flex flex-col p-4">
         <div class="flex justify-start items-center gap-x-2">
-          <UAvatar :src="user.profilePicture" size="xl"/>
+          <UAvatar :src="user.profilePicture" size="xl" />
           <div class="flex flex-col justify-start items-start">
             <p class="text-xl">{{ user.name }} {{ user.surname }}</p>
             <p class="text-sm">{{ user.username }}</p>
@@ -12,7 +12,7 @@
           <div class="flex justify-end items-center flex-grow flex-1 gap-x-2">
             <NuxtLink to="/notifications">
               <UChip :text="unreadCount" size="3xl">
-                <Icon name="material-symbols:notifications-sharp" size="30"/>
+                <Icon name="material-symbols:notifications-sharp" size="30" />
               </UChip>
             </NuxtLink>
           </div>
@@ -20,32 +20,26 @@
 
       </div>
       <div class="p-4" v-if="user.accountType === 'Driver'">
-        <UAlert
-            title="Merhaba Sürücü!"
-            description="Hemen sürüş modunu başlatarak, iş almaya başlayabilirsin."
-            color="neutral"
-            icon="ri:steering-2-line"
-            variant="subtle"
-            :actions="[
-      {
-        label: 'Sürüş moduna geçiş yap',
-         onClick(event) {
-          onVehicleModeClick();
-        },
-      },
-    ]"
-        />
+        <UAlert title="Merhaba Sürücü!" description="Hemen sürüş modunu başlatarak, iş almaya başlayabilirsin."
+          color="neutral" icon="ri:steering-2-line" variant="subtle" :actions="[
+            {
+              label: 'Sürüş moduna geçiş yap',
+              onClick(event) {
+                onVehicleModeClick();
+              },
+            },
+          ]" />
 
       </div>
       <div class="flex flex-col p-4">
         <p>Favori Rotalarınız</p>
         <div class="px-4 py-2 bg-gray-800 text-md rounded-md flex justify-between items-center"
-             v-for="favorite in favoriteTrips">
+          v-for="favorite in favoriteTrips">
           <p>{{ favorite.name }}</p>
           <UButton color="neutral" variant="outline">Seç</UButton>
         </div>
       </div>
-      <BasicTransfer/>
+      <BasicTransfer />
     </div>
 
 
@@ -58,7 +52,7 @@
             öğrenebilirsin.</p>
           <div class="flex flex-col gap-y-2 mt-2">
             <div class="text-black flex gap-x-4 justify-between items-center p-2 rounded-md"
-                 v-for="trip in liveTrips.slice(0,1)">
+              v-for="trip in liveTrips.slice(0, 1)">
               <div class="w-1/2 flex flex-col">
                 <p>{{ trip.name }}</p>
               </div>
@@ -92,50 +86,71 @@
     <UDrawer should-scale-background :direction="'bottom'" v-model:open="isVehicleCardVisible">
       <template #body>
         <div class="flex flex-col rounded-md p-2 mt-2">
-          <img ref="parallaxSource" :src="accountVehicleCard?.photo" :style="cardStyle" alt="vehicle-photo" class="w-full">
+          <p class="text-xl font-bold text-center">{{ accountVehicleCard?.name }}</p>
+          <div class="flex justify-center items-center gap-x-4 mt-2">
+            <UBadge color="neutral" size="lg" variant="subtle" :label="accountVehicleCard?.plate" />
+            <UBadge color="neutral" variant="subtle" size="lg" label="Çevrimdışı"
+              v-if="accountVehicleCard?.status === IVehicleStatus.Offline" />
+            <UBadge color="success" variant="subtle" size="lg" label="Çevrimiçi"
+              v-if="accountVehicleCard?.status === IVehicleStatus.Online" />
+            <UBadge color="warning" variant="subtle" size="lg" label="Bakımda"
+              v-if="accountVehicleCard?.status === IVehicleStatus.InMaintenance" />
+            <UBadge color="error" variant="subtle" size="lg" label="Araç Meşgul"
+              v-if="accountVehicleCard?.status === IVehicleStatus.Busy" />
+          </div>
+          <img :src="accountVehicleCard?.photo" alt="vehicle-photo" class="w-full">
+
+          <UAlert color="warning" variant="subtle" title="Araç bakımda!"  v-if="accountVehicleCard?.status === IVehicleStatus.InMaintenance" 
+            description="Seçmiş olduğunuz araç bakımda gözüküyor, aracı bakımdan çıkarmak istediğinize emin misiniz?"
+            icon="si:warning-duotone" />
+            <UAlert color="error" variant="subtle" title="Araç kullanımda!"  v-if="accountVehicleCard?.status === IVehicleStatus.Busy"
+            description="Bu araç şu anda bir transfer gerçekleştiriyor, araç kullanımını kendi üzerinize almak istediğinize emin misiniz?"
+            icon="si:warning-duotone" />
+            <UAlert color="neutral" variant="subtle" title="Araç kullanımda!"  v-if="accountVehicleCard?.status === IVehicleStatus.Online"
+            description="Bu araç bir başka sürücü ile çevrim içi durumda. Araç kullanımını kendi üzerinize almak istediğinize emin misiniz?"
+            icon="si:warning-duotone" />
         </div>
+      </template>
+      <template #footer>
+        <div class="flex justify-center items-center gap-x-2">
+          <UButton aria-current-value="false" block class="text-center" label="Aracı Benimle İlişkilendir" @click="activateVehicleMode"/>
+          <UButton color="neutral" variant="soft" block class="text-center" aria-current-value="false" label="İptal Et" @click="isVehicleCardVisible=false" />
+        </div>
+
+
       </template>
 
     </UDrawer>
-
 
   </div>
 
 </template>
 
 <script setup lang="ts">
-import {TripStatus} from "~/core/api/modules/trip/models/ITripHeaderDto";
-import {useApi} from "~/core/api/useApi";
-import type {IAccountVehicleDto} from "~/core/api/modules/accountVehicle/models/IAccountVehicle";
-
+import { TripStatus } from "~/core/api/modules/trip/models/ITripHeaderDto";
+import { useApi } from "~/core/api/useApi";
+import type { IAccountVehicleDto } from "~/core/api/modules/accountVehicle/models/IAccountVehicle";
+import { IVehicleStatus } from "~/core/api/modules/accountVehicle/models/IUpdateVehicleStatusCommand";
 let interval: any;
 
 
-const {tripHeaders} = storeToRefs(useCustomerTripStore());
-const {accountVehicleId} = storeToRefs(useVehicleModeStore())
-const {unreadCount} = storeToRefs(useNotificationStore())
-const {user} = storeToRefs(useAuthStore())
-const {favoriteTrips} = storeToRefs(useCreateTransferStore())
+const { tripHeaders } = storeToRefs(useCustomerTripStore());
+const { accountVehicleId } = storeToRefs(useVehicleModeStore())
+const { unreadCount } = storeToRefs(useNotificationStore())
+const { user } = storeToRefs(useAuthStore())
+const { favoriteTrips } = storeToRefs(useCreateTransferStore())
 const accountVehicleCard = ref<IAccountVehicleDto | null>(null);
 const isVehicleCardVisible = ref(false);
-const parallaxSource =ref();
-const {roll, tilt} = useParallax(parallaxSource.value)
-const cardStyle = computed(() => ({
-  transition: '.3s ease-out all',
-  transform: `rotateX(${roll.value * 50}deg) rotateY(${
-    tilt.value * 50
-  }deg)`,
-}))
+
 
 const liveTrips = computed(() => tripHeaders.value.filter(x => x.status == TripStatus.Live));
 const incomingTrips = computed(() => tripHeaders.value.filter(x => x.status == TripStatus.Approved))
 
-const {goTripDetails, getTripHeaders} = useCustomerTripStore();
+const { goTripDetails, getTripHeaders } = useCustomerTripStore();
 
 onMounted(() => {
   useApi().accountVehicle.GetAccountVehicle('4c1bc9fd-b152-448e-8813-9e47b39541ea').then(res => {
-    accountVehicleCard.value = res.data;
-    isVehicleCardVisible.value = true;
+
   })
   getTripHeaders();
   interval = setInterval(() => {
@@ -148,16 +163,20 @@ onUnmounted(() => {
 })
 
 function onVehicleModeClick() {
-  if (accountVehicleId.value != ''){
+  if (accountVehicleId.value != '') {
     useRouter().push('/vehicle-mode')
-  }else{
+  } else {
     useCamera('qr').then(res => {
       useApi().accountVehicle.GetAccountVehicle(res).then(res => {
-
+        accountVehicleCard.value = res.data;
+        isVehicleCardVisible.value = true;
       })
-      useVehicleModeStore().setAccountVehicleId(res)
     })
   }
+}
+
+function activateVehicleMode(){
+  useVehicleModeStore().setAccountVehicleId(accountVehicleCard.value!.id)
 }
 
 
