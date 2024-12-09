a9350cb5c054b2610f35f405af435e0eaadefe89
erdogansonmez <erdogansonmez@outlook.com>
2024-12-09 19:45:42 +0300

    latest

--- pages/complete-profile.vue
+++ pages/complete-profile.vue
@@ -4,15 +4,15 @@
     <UStepper :model-value="activeStep" disabled :items="steps" />
 
     <div class="flex flex-col mt-5 h-full">
-      <ProfilePictureUploader @on-uploaded="nextStep" v-if="activeStep == 0"/>
-      <AddAccountAddress @on-completed="nextStep" :is-default="true" v-if="activeStep == 1"/>
+      <ProfilePictureUploader @on-uploaded="nextStep()" v-if="activeStep == 0"/>
+      <AddAccountAddress @on-completed="nextStep()" :is-default="true" v-if="activeStep == 1"/>
       <div class="flex flex-col items-center justify-center gap-y-4 mt-10" v-if="activeStep == 2">
         <div class="flex flex-col justify-center items-center">
           <p class="text-2xl">Artık Hazırsınız</p>
           <p class="text-sm opacity-50 text-center">Sizde transferciniz dünyasının avantajlı yolculuklarından faydalanabilirsiniz.</p>
         </div>
 
-        <UButton label="Uygulamayı Kullanmaya Başlayın" color="success" variant="subtle" @click="onCompleted"/>
+        <UButton label="Uygulamayı Kullanmaya Başlayın" color="success" variant="subtle" @click="onCompleted()"/>
       </div>
     </div>
   </div>
@@ -39,7 +39,7 @@ const steps = ref([
 const activeStep = ref(0)
 
 function nextStep(){
-  activeStep.value = activeStep.value++
+  activeStep.value = activeStep.value + 1
 }
 
 function onCompleted(){
