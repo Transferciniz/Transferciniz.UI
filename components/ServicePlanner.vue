1a6fd4693927c49b464da4f4a20f8c56ab59b6ea
erdogansonmez <erdogansonmez@outlook.com>
2024-12-14 19:48:28 +0300

    update

--- components/ServicePlanner.vue
+++ components/ServicePlanner.vue
@@ -13,11 +13,22 @@
     </div>
 
     <div class="flex justify-start items-center gap-x-2 my-4">
+      <p class="text-lg">Kişileri Seçin</p>
+      <USeparator orientation="horizontal" class="flex-1" />
+    </div>
+    <div class="flex justify-start items-center flex-wrap gap-x-2 gap-y-2" v-if="selectedEmployee.length > 0" @click="isPeopleEditDrawerVisible = true">
+      <UAvatar v-for="user in selectedEmployee" :src="user.profilePicture" :alt="`${user.name} ${user.surname}`" size="3xl" />
+    </div>
+    <div class="flex flex-col justify-center items-center p-4 bg-gray-800 rounded-md" v-else @click="isPeopleEditDrawerVisible = true">
+      <p class="text-xs">Hiç Kişi Eklememişsiniz, Eklemek için dokunun.</p>
+    </div>
+
+    <div class="flex justify-start items-center gap-x-2 my-4">
       <p class="text-lg">Servis Yönü Seçin</p>
       <USeparator orientation="horizontal" class="flex-1" />
     </div>
     <div class="flex justify-center gap-x-2 items-stretch rounded-md">
-      <div class="flex flex-col items-center justify-center p-4 border border-gray-500/0 rounded-md flex-1" :class="{'bg-gray-700 border !border-gray-500' : directionType == 'oneWay'}" @click="directionType = 'oneWay'">
+      <div class="flex flex-col items-center justify-center p-4 border border-gray-500/0 rounded-md flex-1" :class="{'bg-gray-700 border !border-gray-500' : directionType == 'oneWay'}" @click="directionType = 'oneWay'; isOneWayDetailDrawerVisible = true;">
         <div class="flex justify-start items-center gap-x-2">
           <Icon name="line-md:arrow-up" mode="svg" size="30"/>
           <p class="text-xl">Tek Yön</p>
@@ -32,21 +43,50 @@
         <p class="text-xs opacity-50 text-center mt-2">Hem toplama hem dağıtım servisi yapmak istiyorsanız.</p>
       </div>
     </div>
-    <div class="flex flex-col bg-gray-500 rounded-md mt-[10px]" v-if="directionType != 'none'">
-      <div class="flex justify-between items-center relative top-[-29px]">
-        <div class="flex flex-1 items-center justify-center">
-          <Icon name="ic:sharp-arrow-drop-up" size="50" class="bg-gray-500/0" :class="{'!bg-gray-500' : directionType == 'oneWay'}"/>
-        </div>
-      
-        <div class="flex flex-1 items-center justify-center">
-          <Icon name="ic:sharp-arrow-drop-up" size="50" class="bg-gray-500/0" :class="{'!bg-gray-500' : directionType == 'twoWay'}"/>
+
+    <div class="flex justify-start items-center gap-x-2 my-4" v-if="directionType == 'oneWay' && oneWayDetail != 'none'">
+      <p class="text-lg">Tarih ve Saat</p>
+      <USeparator orientation="horizontal" class="flex-1" />
+    </div>
+    <div class="flex flex-col items-start justify-start" v-if="directionType == 'oneWay' && oneWayDetail != 'none'">
+
+      <div class="flex justify-between items-center w-full">
+        <p class="w-1/2">Tarih:</p>
+        <UPopover>
+          <!--
+           {{ df.format(modelValue.toDate(getLocalTimeZone())) }}
+          -->
+          <UButton color="neutral" variant="subtle" icon="i-lucide-calendar" block>
+            Tarih Seçin
+          </UButton>
+
+          <template #content>
+            <UCalendar :multiple="true" v-model="calendarDates" class="p-2" />
+          </template>
+        </UPopover>
+      </div>
+
+      <div class="flex justify-start items-center w-full mt-2" v-if="directionType == 'oneWay'">
+        <p v-if="oneWayDetail == 'from'" class="w-1/2">Varış Saati Seçin:</p>
+        <p v-if="oneWayDetail == 'to'" class="w-1/2">Çıkış Saati Seçin:</p>
+        <div class="flex justify-end items-center w-full gap-x-2">
+          <USelect :items="hours" class="flex-1"/>
+          <p>:</p>
+          <USelect :items="minutes" class="flex-1"/>
         </div>
       </div>
+
+
+
+
+
+
     </div>
+   
 
   </div>
   <UDrawer title="Kişilerinizde arayın" description="Servisinizi kullanacak kişileri ayarların"
-    v-model:open="isPeopleEditDrawerVisible" direction="bottom">
+    v-model:open="isPeopleEditDrawerVisible" direction="top">
     <template #body>
       <div class="flex flex-col w-full gap-y-2 items-center flex-center">
         <UInput type="text" v-model:model-value="employeeSearchInput" class="w-full"
@@ -71,6 +111,18 @@
         @click="isPeopleEditDrawerVisible = false" />
     </template>
   </UDrawer>
+  <UDrawer title="Tek Yön Servis Tipini Seçin" v-model:open="isOneWayDetailDrawerVisible" direction="top" :handle="false" >
+    <template #body>
+      <div class="flex flex-col gap-y-1 my-2 border border-white/10 p-2 rounded-md" @click="setOneWayDetail('from')">
+        <p class="text-lg">Toplama Servisi</p>
+        <p class="text-xs opacity-50">Kişilerinizi konumlarından alarak, servis konumuna taşıyın.</p>
+      </div>
+      <div class="flex flex-col gap-y-1 my-2 border border-white/10 p-2 rounded-md" @click="setOneWayDetail('to')">
+        <p class="text-lg">Dağıtım Servisi</p>
+        <p class="text-xs opacity-50">Kişilerinizi servis konumundan başlayarak, konumlarına taşıyın.</p>
+      </div>
+    </template>
+  </UDrawer>
   <LocationSelector v-model:open="isLocationSelectorVisible" @on-location-selected="onLocationSelected" />
 </template>
 
@@ -78,6 +130,7 @@
 import type { IEmployee } from '~/core/api/modules/account/models/IEmployee';
 import { useApi } from '~/core/api/useApi';
 import type { ILocationSearchResult } from '~/core/app/ITripLocation';
+import { CalendarDate, DateFormatter, getLocalTimeZone } from '@internationalized/date'
 
 const {
   address,
@@ -95,9 +148,23 @@ const employeeView = computed(() => {
   return employeeDataSource.value.toSorted((a, b) => (a.isSelected ? 1 : 0) - (b.isSelected ? 1 : 0))
 });
 
+const selectedEmployee = computed(() => employeeDataSource.value.filter(x => x.isSelected))
+
+const hours = ref([
+  1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+])
+const minutes = ref([
+  0, 10, 15, 20,30,40,45,50,60
+])
+
 const directionType = ref<'oneWay' | 'twoWay' | 'none'>('none');
+const oneWayDetail = ref<'to' | 'from' | 'none'>('none');
+const isOneWayDetailDrawerVisible = ref(false);
 
-
+function setOneWayDetail(payload: 'to' | 'from'){
+  oneWayDetail.value = payload;
+  isOneWayDetailDrawerVisible.value = false;
+}
 function toggleEmployee(payload: IEmployee) {
   employeeDataSource.value.find(x => x == payload)!.isSelected = !employeeDataSource.value.find(x => x == payload)!.isSelected
 }
@@ -107,6 +174,13 @@ function onLocationSelected(payload: ILocationSearchResult): void {
   isLocationSelectorVisible.value = false;
 }
 
+
+
+const df = new DateFormatter('tr-TR', {
+  dateStyle: 'medium'
+})
+
+const calendarDates = ref([])
 
 
 
